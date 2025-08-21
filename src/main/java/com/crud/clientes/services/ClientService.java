86017a7f0ead08bcb9c980a797a4d30937496cf5
Allana Avila <allanavila@gmail.com>
2025-08-21 09:53:55 -0300

    busca por id e existente e inexiste

---
+++ src/main/java/com/crud/clientes/services/ClientService.java
@@ -0,0 +1,25 @@
+package com.crud.clientes.services;
+
+import com.crud.clientes.dto.ClientDTO;
+import com.crud.clientes.entities.Client;
+import com.crud.clientes.repositories.ClientRepository;
+import com.crud.clientes.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Client client = clientRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Recurso não encontrado"));
+        return new ClientDTO(client);
+    }
+}
